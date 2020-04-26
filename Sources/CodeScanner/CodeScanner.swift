6a5bca259f85e129c10fdd258096ab39a85a5898
Paul Hudson <paul.hudson@gmail.com>
2020-04-26 10:49:36 +0100

    Simplified the UI.

--- Sources/CodeScanner/CodeScanner.swift
+++ Sources/CodeScanner/CodeScanner.swift
@@ -16,38 +16,38 @@ public struct CodeScannerView: UIViewControllerRepresentable {
     public enum ScanError: Error {
         case badInput, badOutput
     }
-    
+
     public class ScannerCoordinator: NSObject, AVCaptureMetadataOutputObjectsDelegate {
         var parent: CodeScannerView
         var codeFound = false
-        
+
         init(parent: CodeScannerView) {
             self.parent = parent
         }
-        
+
         public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
             if let metadataObject = metadataObjects.first {
                 guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
                 guard let stringValue = readableObject.stringValue else { return }
                 guard codeFound == false else { return }
-                
+
                 AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
                 found(code: stringValue)
-                
+
                 // make sure we only trigger scans once per use
                 codeFound = true
             }
         }
-        
+
         func found(code: String) {
             parent.completion(.success(code))
         }
-        
+
         func didFail(reason: ScanError) {
             parent.completion(.failure(reason))
         }
     }
-    
+
     #if targetEnvironment(simulator)
     public class ScannerViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
         var delegate: ScannerCoordinator?
@@ -57,38 +57,25 @@ public struct CodeScannerView: UIViewControllerRepresentable {
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
             label.numberOfLines = 0
-            
-            label.text = "You're running in the simulator, which means the camera isn't available."
+
+            label.text = "You're running in the simulator, which means the camera isn't available. Tap anywhere to send back some simulated data."
             label.textAlignment = .center
             let button = UIButton()
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.setTitle("Select Image from Gallery", for: .normal)
+            button.setTitle("Or tap here to select a custom image", for: .normal)
             button.setTitleColor(UIColor.systemBlue, for: .normal)
             button.setTitleColor(UIColor.gray, for: .highlighted)
             button.addTarget(self, action: #selector(self.openGallery), for: .touchUpInside)
-            
-            
-            
+
             let stackView = UIStackView()
             stackView.translatesAutoresizingMaskIntoConstraints = false
             stackView.axis = .vertical
             stackView.spacing = 50
             stackView.addArrangedSubview(label)
             stackView.addArrangedSubview(button)
-            
-            //Adding mock button if smaple data is available
-            if !(delegate?.parent.simulatedData.isEmpty ?? true){
-                let mockButton = UIButton()
-                mockButton.translatesAutoresizingMaskIntoConstraints = false
-                mockButton.setTitle("Mock scan with sample data", for: .normal)
-                mockButton.setTitleColor(UIColor.systemBlue, for: .normal)
-                mockButton.setTitleColor(UIColor.gray, for: .highlighted)
-                mockButton.addTarget(self, action: #selector(self.mockWithSample), for: .touchUpInside)
-                stackView.addArrangedSubview(mockButton)
-            }
-            
+
             view.addSubview(stackView)
-            
+
             NSLayoutConstraint.activate([
                 button.heightAnchor.constraint(equalToConstant: 50),
                 stackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
@@ -96,32 +83,33 @@ public struct CodeScannerView: UIViewControllerRepresentable {
                 stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
             ])
         }
-        
+
+        override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            guard let simulatedData = delegate?.parent.simulatedData else {
+                print("Simulated Data Not Provided!")
+                return
+            }
+
+            delegate?.found(code: simulatedData)
+        }
+
         @objc func openGallery(_ sender: UIButton){
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             self.present(imagePicker, animated: true, completion: nil)
         }
-        
-        @objc func mockWithSample(_ sender: UIButton){
-            guard let simulatedData = delegate?.parent.simulatedData else {
-                print("Simulated Data Not Provided!")
-                return
-            }
-            delegate?.found(code: simulatedData)
-        }
-        
+
         public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
             if let qrcodeImg = info[.originalImage] as? UIImage {
                 let detector:CIDetector=CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [CIDetectorAccuracy:CIDetectorAccuracyHigh])!
                 let ciImage:CIImage=CIImage(image:qrcodeImg)!
                 var qrCodeLink=""
-                
+
                 let features=detector.features(in: ciImage)
                 for feature in features as! [CIQRCodeFeature] {
                     qrCodeLink += feature.messageString!
                 }
-                
+
                 if qrCodeLink=="" {
                     delegate?.didFail(reason: .badOutput)
                 }else{
@@ -143,12 +131,12 @@ public struct CodeScannerView: UIViewControllerRepresentable {
         override public func viewDidLoad() {
             super.viewDidLoad()
 
-            
+
             NotificationCenter.default.addObserver(self,
                                                    selector: #selector(updateOrientation),
                                                    name: Notification.Name("UIDeviceOrientationDidChangeNotification"),
                                                    object: nil)
-            
+
             view.backgroundColor = UIColor.black
             captureSession = AVCaptureSession()
 
@@ -184,7 +172,7 @@ public struct CodeScannerView: UIViewControllerRepresentable {
         override public func viewWillLayoutSubviews() {
             previewLayer.frame = view.layer.bounds
         }
-        
+
         @objc func updateOrientation() {
             guard let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation else {
                 return
@@ -202,7 +190,7 @@ public struct CodeScannerView: UIViewControllerRepresentable {
             updateOrientation()
             captureSession.startRunning()
         }
-        
+
         override public func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
 
@@ -217,7 +205,7 @@ public struct CodeScannerView: UIViewControllerRepresentable {
             if (captureSession?.isRunning == true) {
                 captureSession.stopRunning()
             }
-            
+
             NotificationCenter.default.removeObserver(self)
         }
 
@@ -230,29 +218,29 @@ public struct CodeScannerView: UIViewControllerRepresentable {
         }
     }
     #endif
-    
+
     public let codeTypes: [AVMetadataObject.ObjectType]
     public var simulatedData = ""
     public var completion: (Result<String, ScanError>) -> Void
-    
+
     public init(codeTypes: [AVMetadataObject.ObjectType], simulatedData: String = "", completion: @escaping (Result<String, ScanError>) -> Void) {
         self.codeTypes = codeTypes
         self.simulatedData = simulatedData
         self.completion = completion
     }
-    
+
     public func makeCoordinator() -> ScannerCoordinator {
         return ScannerCoordinator(parent: self)
     }
-    
+
     public func makeUIViewController(context: Context) -> ScannerViewController {
         let viewController = ScannerViewController()
         viewController.delegate = context.coordinator
         return viewController
     }
-    
+
     public func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {
-        
+
     }
 }
 
