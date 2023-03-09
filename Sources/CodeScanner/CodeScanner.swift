6e3114415fed3500aa4b003676fc82568f623fec
NathanFallet <contact@nathanfallet.me>
2023-03-09 14:00:09 +0100

    Moving extension and fixing missing available annotation

--- Sources/CodeScanner/CodeScanner.swift
+++ Sources/CodeScanner/CodeScanner.swift
@@ -115,16 +115,6 @@ public struct CodeScannerView: UIViewControllerRepresentable {
     
 }
 
-public extension AVCaptureDevice {
-    
-    /// This returns the Ultra Wide Camera on capable devices and the default Camera for Video otherwise.
-    static var bestForVideo: AVCaptureDevice? {
-        let deviceHasUltraWideCamera = !AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInUltraWideCamera], mediaType: .video, position: .back).devices.isEmpty
-        return deviceHasUltraWideCamera ? AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) : AVCaptureDevice.default(for: .video)
-    }
-    
-}
-
 @available(macCatalyst 14.0, *)
 struct CodeScannerView_Previews: PreviewProvider {
     static var previews: some View {
